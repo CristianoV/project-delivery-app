854fa8afd650a7e84422948bd55c35a4d92a32e7
CristianoV <cristianoviieira@gmail.com>
2022-10-27 19:19:49 -0300

    fix: erro de lint no index

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,7 +11,7 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
