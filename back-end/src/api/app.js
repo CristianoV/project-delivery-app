5592e06f589d78e6057d5e201cb2cc4adeec40f5
CristianoV <cristianoviieira@gmail.com>
2022-10-29 06:28:11 -0300

    feat: adicionado nova rota e validação para acessar rota

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const clientController = require('../controller/clientController');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.get('/products', clientController.getAllProducts);
+
 
 module.exports = app;
