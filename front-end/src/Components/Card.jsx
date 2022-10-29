788ffb3962379b085e2cf830747f7104309bb3f8
CristianoV <cristianoviieira@gmail.com>
2022-10-29 04:34:29 -0300

    feat: adiciona component Card

---
+++ front-end/src/Components/Card.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function Card({ image, title, price, id }) {
+  const [quantity, setQuantity] = useState(0);
+  return (
+    <div>
+      <img
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
+        src={ image }
+        alt={ title }
+      />
+      <div>
+        <h3 data-testid={ `customer_products__element-card-title-${id}` }>{title}</h3>
+        <p data-testid={ `customer_products__element-card-price-${id}` }>
+          {price}
+        </p>
+        <div>
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
+            onClick={ () => quantity !== 0 && setQuantity(quantity - 1) }
+          >
+            -
+          </button>
+          <span
+            data-testid={ `customer_products__input-card-quantity-${id}` }
+          >
+            {quantity}
+          </span>
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-add-item-${id}` }
+            onClick={ () => setQuantity(quantity + 1) }
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default Card;
