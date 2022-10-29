ca9f08985f03ccb19b3b9f170e681843edc3928b
CristianoV <cristianoviieira@gmail.com>
2022-10-29 04:35:47 -0300

    feat: adiciona cards na rota de clients

--- front-end/src/Pages/Products.jsx
+++ front-end/src/Pages/Products.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
 import Navbar from '../Components/Navbar';
+import Card from '../Components/Card';
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  async function getUserAccount() {
+    const { results } = await (await Axios.get('https://rickandmortyapi.com/api/character')).data;
+    return results;
+  }
+
+  useEffect(() => {
+    getUserAccount().then((x) => setProducts(x));
+  }, []);
+
   return (
     <div>
-      <Navbar page="products" />
+      <Navbar />
+      {/* {products && products.map((item) => (
+        <Card
+          image={ item.urlImage }
+          title={ item.name }
+          price={ item.price }
+          id={ item.id }
+          key={ item.id }
+        />
+      ))} */}
+      {products && products.map((item) => (
+        <Card
+          image={ item.image }
+          title={ item.name }
+          price={ item.species }
+          id={ item.status }
+          key={ item.name }
+        />
+      ))}
     </div>
   );
 }
