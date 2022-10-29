68b14a94a85e4d91fef27d65f0323192c1e410ed
CristianoV <cristianoviieira@gmail.com>
2022-10-29 06:43:03 -0300

    fix: erro de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,13 +4,12 @@ const clientController = require('../controller/clientController');
 const app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.get('/products', clientController.getAllProducts);
-
 
 module.exports = app;
--- back-end/src/controller/clientController.js
+++ back-end/src/controller/clientController.js
@@ -1,10 +1,10 @@
-const clientService = require("../service/clientService");
+const clientService = require('../service/clientService');
 
 const clientController = {
-    getAllProducts: async (req, res) => {
-        const products = await clientService.getAllProducts();
-        return res.status(200).json(products);
-    }
-}
+  getAllProducts: async (_req, res) => {
+    const products = await clientService.getAllProducts();
+    return res.status(200).json(products);
+  },
+};
 
-module.exports =  clientController;
+module.exports = clientController;
