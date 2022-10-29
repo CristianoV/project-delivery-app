4c3b318d02ba22bddd4b753ffb1ca1770688c1c4
CristianoV <cristianoviieira@gmail.com>
2022-10-29 07:16:17 -0300

    fix: lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,7 +3,7 @@ const clientController = require('../controller/clientController');
 
 const app = express();
 
-app.use(function(req, res, next) {
+app.use(function(_req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
