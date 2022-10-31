07445f220e30f09297a984da4d5fb11095437265
CristianoV <cristianoviieira@gmail.com>
2022-10-31 17:11:11 -0300

    fix: controller retornando token

--- back-end/src/controller/login.js
+++ back-end/src/controller/login.js
@@ -2,17 +2,17 @@ const loginService = require('../service/login');
 
 const login = async (req, res) => {
    const { email, password } = req.body;
-   const user = await loginService.login(email, password);
+   const token = await loginService.login(email, password);
 
-   if (user.message === 'User not found') {
+   if (token.message === 'User not found') {
     return res.status(404).json({ message: 'User not found' });
    }
 
-   if (user.message === 'Invalid password') {
+   if (token.message === 'Invalid password') {
     return res.status(400).json({ message: 'Invalid password' });
    }
 
-   return res.status(200).json(user);
+   return res.status(200).json(token);
 };
 
 module.exports = { login };
