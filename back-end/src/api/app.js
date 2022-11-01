52b301126d2088804e3f3ba7c5ffbe48b8ef799d
CristianoV <cristianoviieira@gmail.com>
2022-11-01 13:20:24 -0300

    fix: singlequote

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-const loginController = require("../controller/login");
-const registerController = require("../controller/register");
-const clientController = require("../controller/clientController");
-const { validateToken } = require("../middleware/validateToken");
+const express = require('express');
+const cors = require('cors');
+const loginController = require('../controller/login');
+const registerController = require('../controller/register');
+const clientController = require('../controller/clientController');
+const { validateToken } = require('../middleware/validateToken');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/images", express.static("public"));
+app.use('/images', express.static('public'));
 
-app.get("/coffee", (_req, res) => res.status(418).end());
-app.get("/products", validateToken, clientController.getAllProducts);
+app.get('/coffee', (_req, res) => res.status(418).end());
+app.get('/products', validateToken, clientController.getAllProducts);
 
-app.post("/login", loginController.login);
-app.post("/register", registerController.register);
+app.post('/login', loginController.login);
+app.post('/register', registerController.register);
 
 module.exports = app;
