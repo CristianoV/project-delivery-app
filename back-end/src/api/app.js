e20c0eca71320c7ee70cc64e7caf4b757888abb4
CristianoV <cristianoviieira@gmail.com>
2022-11-01 12:17:56 -0300

    feat: adiciona cors e middleware que valida token

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,21 +1,19 @@
 const express = require('express');
+var cors = require('cors')
 const loginController = require('../controller/login');
 const registerController = require('../controller/register');
 const clientController = require('../controller/clientController');
+const {validateToken} = require('../middleware/validateToken');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 
 app.use('/images', express.static('public'));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
-app.get('/products', clientController.getAllProducts);
+app.get('/products',  validateToken, clientController.getAllProducts);
 
 app.post('/login', loginController.login);
 app.post('/register', registerController.register);
