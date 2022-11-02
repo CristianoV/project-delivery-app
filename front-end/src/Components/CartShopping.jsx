23be5325071ade0c09a37da5ce97d94a674f5164
CristianoV <cristianoviieira@gmail.com>
2022-11-02 18:50:50 -0300

    feat: adiciona botão para acessar o carrinho de compras

---
+++ front-end/src/Components/CartShopping.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import MyContext from '../context/store';
+
+function CartShopping() {
+  const [total, setTotal] = useState(0);
+  const theme = useContext(MyContext);
+  const navigation = useNavigate();
+  const priceFormat = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  useEffect(() => {
+    const cart = theme.saller.reduce((acc, item) => {
+      const { quantity, price } = item;
+      return acc + (quantity * price);
+    }, 0);
+    setTotal(cart);
+    sessionStorage.setItem('cart', JSON.stringify(theme.saller));
+  }, [theme]);
+
+  return (
+    <div>
+      <button
+        type="button"
+        data-testid="customer_products__checkout-bottom-value"
+        onClick={ () => navigation('/customer/checkout') }
+        // disabled={ total === 0 }
+      >
+        {`Preço total: ${priceFormat.format(total)}`}
+      </button>
+    </div>
+  );
+}
+
+export default CartShopping;
