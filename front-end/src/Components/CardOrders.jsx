9787c07ebae9d3b6d52d476733befceef4835b1e
davizinho98 <daviseles99@gmail.com>
2022-11-04 15:16:04 -0500

    feat(frontend): implementa botão pra redirecionar pra pagina de detalhe do pedido e formatação da data e preços

--- front-end/src/Components/CardOrders.jsx
+++ front-end/src/Components/CardOrders.jsx
@@ -1,22 +1,33 @@
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 function CardOrders({ orderId, statusOrder, date, totalPrice }) {
+  const navigate = useNavigate();
+  const n = new Date(date);
+  const priceFormat = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  });
   return (
-    <div>
-      <span data-testid={ `customer_orders__element-order-id-${orderId}` }>
+    <button
+      type="button"
+      data-testid$={ `${orderId}` }
+      onClick={ () => navigate(`/customer/orders/${orderId}`) }
+    >
+      <div data-testid={ `customer_orders__element-order-id-${orderId}` }>
         {/* Verificar se é esse testid */}
-        { orderId + 1}
-      </span>
-      <span data-testid={ `customer_orders__element-delivery-status-${orderId}` }>
+        { orderId }
+      </div>
+      <div data-testid={ `customer_orders__element-delivery-status-${orderId}` }>
         { statusOrder }
-      </span>
-      <span data-testid={ `customer_orders__element-order-date-${orderId}` }>
-        { date }
-      </span>
-      <span data-testid={ `customer_orders__element-card-price-${orderId}` }>
-        { totalPrice }
-      </span>
-    </div>
+      </div>
+      <div data-testid={ `customer_orders__element-order-date-${orderId}` }>
+        { n.toLocaleDateString('pt-BR') }
+      </div>
+      <div data-testid={ `customer_orders__element-card-price-${orderId}` }>
+        {priceFormat.format(totalPrice)}
+      </div>
+    </button>
   );
 }
 
