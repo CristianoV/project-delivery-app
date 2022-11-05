3af2984f3f0c5e1a17b6a4398fa25e790e8efe34
CristianoV <cristianoviieira@gmail.com>
2022-11-05 03:15:53 -0300

    feat: pequenas atualizações no model e migrations

--- back-end/src/database/migrations/05-sales-products.js
+++ back-end/src/database/migrations/05-sales-products.js
@@ -3,28 +3,28 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('sales_products', {
       saleId: {
-        primaryKey: true,
         allowNull: false,
         type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
         field: 'sale_id',
         references: {
           model: 'sales',
           key: 'id'
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
       productId: {
-        primaryKey: true,
         allowNull: false,
         type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
         field: 'product_id',
         references: {
           model: 'products',
           key: 'id'
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
       quantity: {
         type: Sequelize.INTEGER,
--- back-end/src/database/models/sales-products.js
+++ back-end/src/database/models/sales-products.js
@@ -2,12 +2,24 @@ module.exports = (sequelize, DataTypes) => {
   const SaleProduct = sequelize.define('SaleProduct', {
     saleId: {
       type: DataTypes.INTEGER,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
       primaryKey: true,
+      references: {
+        model: 'sales',
+        key: 'id',
+      },
       
     }, 
     productId: {
       type: DataTypes.INTEGER,
       primaryKey: true,
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      references: {
+        model: 'products',
+        key: 'id',
+      },
     },
     quantity: {
       type: DataTypes.INTEGER,
@@ -17,18 +29,18 @@ module.exports = (sequelize, DataTypes) => {
   );
 
   SaleProduct.associate = (models) => {
-    models.Product.belongsToMany(models.Sale, {
-      as: 'salesProduct',
-      through: SaleProduct,
-      foreignKey: 'sale_id',
-      otherKey: 'product_id',
+    SaleProduct.belongsTo(models.Sale, {
+      as: 'sales',
+      // through: SaleProduct,
+      foreignKey: 'saleId',
+      // otherKey: 'product_id',
     });
 
-    models.Sale.belongsToMany(models.Product, {
-      as: 'salesProduct',
-      through: SaleProduct,
-      foreignKey: 'product_id',
-      otherKey: 'sale_id',
+    SaleProduct.belongsTo(models.Product, {
+      as: 'products',
+      // through: SaleProduct,
+      foreignKey: 'productId',
+      // otherKey: 'sale_id',
     });
   };
 
