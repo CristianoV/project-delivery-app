e60362e1b8faaeed1e9fc033724c8d5ed59b0611
CristianoV <cristianoviieira@gmail.com>
2022-11-05 03:12:40 -0300

    feat: adiciona nova função de vendas na camada de controller

--- back-end/src/controller/salesController.js
+++ back-end/src/controller/salesController.js
@@ -6,6 +6,11 @@ const salesController = {
     if (!newSale) return res.status(500).json({ message: 'Erro interno' });
     return res.status(201).json(newSale);
   },
+  selectAllSalesByUserId: async (req, res) => {
+    const { id } = req.params;
+    const sale = await salesService.selectAllSalesByUserId(id)
+    return res.status(201).json(sale);
+  }
 };
 
 module.exports = salesController;
