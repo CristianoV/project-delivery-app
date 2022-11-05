3f7186faee178857d93b0943d3e7c4342328d412
CristianoV <cristianoviieira@gmail.com>
2022-11-05 03:07:14 -0300

    fix: componente renomeado

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,7 @@ import RegisterComponent from './Pages/Register';
 import Products from './Pages/Products';
 import CustomerCheckout from './Pages/CustomerCheckout';
 import MyContext from './context/store';
-import OrderById from './Pages/OrderById';
+import OrderDetail from './Pages/OrderDetail';
 
 function App() {
   const [saller, setSaller] = useState([]);
@@ -32,7 +32,7 @@ function App() {
         <Route exact path="/" element={ <RedirectLogin /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/register" element={ <RegisterComponent /> } />
-        <Route path="/customer/orders/:id" element={ <OrderById /> } />
+        <Route path="/customer/orders/:id" element={ <OrderDetail /> } />
 
         <Route
           path="/customer/products"
