078d6f8f6b82fe52ed81f0e985a059a7a503836c
CristianoV <cristianoviieira@gmail.com>
2022-11-10 11:16:11 -0300

    feat: adiciona tamanho de telas no tailwind

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       colors: {
         primary: '#036B52',
